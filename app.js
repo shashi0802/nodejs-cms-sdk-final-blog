c4f71ad7a61894437ddf82f12cc33b5fbc5c91d2
shashi0802 <shashikant.yadav@contentstack.com>
2018-06-20 20:26:18 +0530

    nunjuks 2nd commit for change

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const request = require('request-promise');
 var app = express();
 
 port = 8000;
@@ -12,19 +13,56 @@ nunjucks.configure('views', {
 
 app.use('/static', express.static('public'))
 
+// starting from here-------
+// const options = {
+//     method: 'GET',
+//     uri: 'https://cdn.contentstack.io/v3/content_types/header/entries/bltafb1e4dba1ec0661?api_key=blt610d3dc8e04d1ec7&access_token=blt9b73134d806fc7b4&environment=preview&locale=en-us&include_workflow=true'
 
+// }
+// request(options)
+//     .then(function (data) {
+//         response.render('home',{result:data});
+//         // Request was successful, use the response object at will
+//     })
+//     .catch(function (err) {
+//         if(err){
+//             throw err;
+//         }
+//         // Something bad happened, handle the error
+//     })
+
+//----------------------------------------------------------------------------------------------------------
 app.get('/', (req, res) => {
+
+    const options = {
+        method: 'GET',
+        uri: 'https://cdn.contentstack.io/v3/content_types/header/entries/bltafb1e4dba1ec0661?api_key=blt610d3dc8e04d1ec7&access_token=blt9b73134d806fc7b4&environment=preview&locale=en-us&include_workflow=true'
     
-    res.render('home');
-    console.log("Hello nunjuks....!");
+    }
+    request(options)
+        .then(function (data) {
+           // var data = {url:"https://images.contentstack.io/v3/assets/blt610d3dc8e04d1ec7/blte3a31295e3858eca/5b28a05fa528cb8f049e9616/logo.png"}
+            data = JSON.parse(data);
+            res.render('home',{data});
+           // console.log(data);
+            // Request was successful, use the response object at will
+        })
+        .catch(function (err) {
+            if(err){
+                throw err;
+            }
+            // Something bad happened, handle the error
+        })
+    // res.render('home');
+     //console.log("Hello nunjuks....!");
 });
 app.get('/faq', (req, res) => {
-    
+
     res.render('faq');
     console.log("Hello nunjuks....!");
 });
 app.get('/registration', (req, res) => {
-    
+
     res.render('registration');
     console.log("Hello nunjuks....!");
 });
